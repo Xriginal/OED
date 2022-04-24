b0a240726b853f50570fa39d1e2dac6f533b6eb4
Xriginal <Oscarruiz25341@gmail.com>
2022-04-23 18:48:54 -0700

    Updating I believe im close

--- src/client/app/containers/MeterDropdownContainer.ts
+++ src/client/app/containers/MeterDropdownContainer.ts
@@ -8,10 +8,8 @@
  import MeterDropdownComponent from '../components/MeterDropDownComponent';
  import { State } from '../types/redux/state';
  import { Dispatch } from '../types/redux/actions';
- import { MeterMetadata, MetersState, } from '../types/redux/meters';
- import { metersInGroup, setIntersect, unitsCompatibleWithMeters } from '../utils/determineCompatibleUnits';
- import meters from 'reducers/meters';
- import { metersApi } from 'utils/api';
+ import { unitsCompatibleWithMeters } from '../utils/determineCompatibleUnits';
+ import { SelectOption } from '../types/items';
  import Meter from '/home/ubermensch/OED/src/server/models/Meter';
  
  /**
@@ -29,16 +27,15 @@
  }
  
  export function getvisibleMeters(state: State) {
-	 let visibleMeters;
-	 if (state.admin) {
+	 const visibleMeters = new Set<Meter>();
+	 if (state.currentUser.profile?.role == 'admin') {
 		 // Can see all meters that don't have null for unit
-		 visibleMeters = Meter.getUnitNotNull
-		 //state.meters.isFetching.valueOf 
+		 //visibleMeters.add(Meter.getUnitNotNull)
+		 visibleMeters.forEach(meter => {meter.id = Meter.getUnitNotNull})
 	 }
- 
 	 else {
 		 // regular user or not logged in so only displayable ones
-		 visibleMeters = Meter.getDisplayable
+		 visibleMeters.forEach(meter => {meter.id = Meter.getDisplayable})
 	 }
 	 // meters that can graph
 	 let compatibleMeters = new Set<number>();
@@ -46,32 +43,53 @@
 	 let incompatibleMeters = new Set<number>();
  
 	 const M = new Set<number>();
+	 	// {M} means turn M into a set.
  
 	 if (state.graph.selectedUnit == -99) {
 		 // If there is no graphic unit then no meters/groups are displayed and you can display all meters.
 		 //  Also, if not admin, then meters not displayable are not viewable.
 		 // admin can see all except if unit is null (not included in ones gotten above). 
-		 compatibleMeters.forEach = visibleMeters;
+		 visibleMeters.forEach(meter => {
+				compatibleMeters.add(meter.displayable);			
+			})
 	 }
 	 // If displayable is false then only admin.
 	 else {
-		 // {M} means turn M into a set.
 		 state.graph.selectedMeters.forEach(meter => {//for each visibleMeters M 
 			 M.add(meter)
 			 const newUnits = unitsCompatibleWithMeters(M)
-			 if (newUnits)//graphicUnit is in units
+			 if (newUnits.has(state.meters.byMeterID[meter].defaultGraphicUnit))//graphicUnit is in units
 			 {
 				 // The compatible units of the meter have graphic unit so can graph (case 1)
-				 compatibleMeters = M;
+				 compatibleMeters.add(meter);
 			 }
 			 else {
 				 // Case 2
-				 incompatibleMeters = M;
+				 incompatibleMeters.add(meter);
 			 }
 		 })
 	 }
 	 //Ready to display meters in menu. Note you display the identifier and not the id.
 	 //For each compatibleUnit C add name C.identifier to meter menu in alphabetically sorted order in regular font for case 1
 	 //ForAdd each incompatibleUnit I add name I.identifier to meter menu in alphabetically sorted order as grayed out and not selectable for case 2
+	 const finalMeter: SelectOption[] = [];
+	 visibleMeters.forEach(meter => {
+		if (compatibleMeters.has(meter.unitId)) {
+			finalMeter.push({
+				label: state.meters.byMeterID[meter.identifier].name,
+				value: meter.name,
+				isDisabled: false
+			} as SelectOption)
+		} else if (incompatibleMeters.has(meter.unitId)) {
+			finalMeter.push({
+				label: state.groups.byGroupID[meter.identifier].name,
+				value: meter.name,
+				isDisabled: true
+			} as SelectOption)
+		}
+	})
+
+	return _.sortBy(_.values(finalMeter), 'label');
  }
+ 
  export default connect(mapStateToProps, mapDispatchToProps)(MeterDropdownComponent);
